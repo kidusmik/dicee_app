569ed50cf034ce888a8faaaea010685e25a743e3
kidusmworku <kidus.m.worku@gmail.com>
2021-11-29 09:59:28 +0300

    Centered and Padded Images

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,23 @@ class DicePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
